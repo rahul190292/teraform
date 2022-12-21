34731d5bfe36cbdb0e4a116386f5798d0f0b90d2
rahul190292 <rahul190292@gmail.com>
2022-12-21 21:56:18 +0530

    first commit

---
+++ ec2-instance.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.2.0"
+  required_providers {
+    aws = {
+      sousource = "hashicorp/aws"
+      version = "4.16"
+  
+    }
+  
+  }
+}
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "myfirstinstance" {
+  instance_type = "t2.micro"
+  ami = "ami-0a606d8395a538502"
+  
+}
